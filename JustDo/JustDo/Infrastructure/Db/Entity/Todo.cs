e6948c0561ddd46ff9715101a4b2e4dd2de14b33
Sergey Yaremchenko <sergey.yarenchenko@gmail.com>
2020-04-12 14:40:00 +0300

    initial commit

---
+++ JustDo/JustDo/Infrastructure/Db/Entity/Todo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustDo.Infrastructure.Db.Entity {
+    public class Todo {
+
+    }
+}
---
+++ JustDo/JustDo/Infrastructure/Db/Entity/TodoContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace JustDo.Infrastructure.Db.Entity {
+    public class TodoContext: DbContext {
+    }
+}
